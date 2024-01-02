727e7c8ea5f288f62c01d817c631d7f714f8193f
zeroesandones01 <z3r0sh1k10707@gmail.com>
2024-01-02 16:36:55 +0800

    Commit sa work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components';
+import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Pie, Financial, ColorPicker, ColorMapping, Editor} from './pages'
 
 import './App.css'
 
@@ -21,12 +23,12 @@ const App = () => {
                 </div>
                 {activeMenu ? (
                     <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'> 
-                        Sidebar
+                        <Sidebar />
                     </div>    
 
                 ): (
                     <div className='w-0 dark:bg-secondary-dark-bg'>
-                        Sidebar-w-0
+                        <Sidebar />
                     </div>    
                 )}
                 <div className={
@@ -36,8 +38,36 @@ const App = () => {
 
                 }>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navvar w-full'>
-                        Navbar
+                        <Navbar />
                     </div>
+                </div>
+                <div>
+                    <Routes>
+                        {/* Dashboard */}
+                        <Route path='/' element="ECommerce"/>
+                        <Route path='/ecommerce' element="ECommerce" />
+
+                        {/* pages */}
+                        <Route path='/orders' element="Orders" />
+                        <Route path='/employees' element="Employee" />
+                        <Route path='/customers' element="Customers" />
+
+                        {/* Apps */}
+                        <Route path='/kanban' element="Kanban" />
+                        <Route path='/editor' element="Editor" />
+                        <Route path='/calendar' element="Calendar" />
+                        <Route path='/color-picker' element="ColorPicker" />
+
+                        {/* Charts */}
+                        <Route path='/line' element="Line" />
+                          <Route path='/area' element="Area" />
+                          <Route path='/bar' element="Bar" />
+                          <Route path='/pie' element="Pie" />
+                          <Route path='/financial' element="Financial" />
+                          <Route path='/color-mapping' element="ColorMapping" />
+                          <Route path='/pyramid' element="Pyramid" />
+                          <Route path='/stacked' element="Stacked" />
+                    </Routes>
                 </div>
             </div>
         </BrowserRouter>
